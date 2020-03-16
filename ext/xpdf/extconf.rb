b63138396bd5cdea767756b706ef792f8c45c1e4
Jakob Kofoed Janot <jakob.janot@karnovgroup.com>
2020-03-16 22:28:45 +0100

    Got version 3.04 working

--- ext/xpdf/extconf.rb
+++ ext/xpdf/extconf.rb
@@ -1,21 +1,21 @@
 require 'mkmf'
-require 'fileutils'
+require 'pathname'
 
-VERSION = File.read(File.join(__dir__, "VERSION"))
-root = File.expand_path('../../..', __FILE__)
+root = Pathname.new(__dir__).join("..", "..").expand_path
+version = root.join("VERSION").read
+xpdf_tar_file = root.join("xpdf-#{version}.tar.gz")
+xpdf_src_dir = root.join("xpdf-#{version}")
 
-FileUtils.mkpath(File.join("vendor"))
-
-Dir.chdir(File.join(root, 'vendor')) do
-  system "wget -quiet https://xpdfreader-dl.s3.amazonaws.com/xpdf-#{VERSION}.tar.gz"
-  system "tar xzf xpdf-#{VERSION}.tar.gz"
+Dir.chdir(root) do
+  system "wget -q https://xpdfreader-dl.s3.amazonaws.com/old/xpdf-#{version}.tar.gz && "\
+    "tar xzf #{xpdf_tar_file}"
 end
 
-Dir.chdir(File.join(root, 'vendor', "xpdf-#{VERSION}")) do
-  system "./configure", "--prefix=#{root}"
-  system "make"
-  system "make install"
-  system "make clean"
+Dir.chdir(xpdf_src_dir) do
+  system "./configure --prefix=#{root} --enable-a4-paper --without-libpaper-library --without-x --without-t1-library && make install"
 end
+
+xpdf_tar_file.delete if xpdf_tar_file.exist?
+xpdf_src_dir.rmtree if xpdf_src_dir.exist?
 
 create_makefile 'xpdf'
